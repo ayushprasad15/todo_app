76ac636219a3c87209ab83d725b39d39cf04461c
ayush <ayush210prasad@gmail.com>
2024-01-29 19:25:51 +0530

    bonus of example compress

--- gui.py
+++ gui.py
@@ -4,6 +4,6 @@ import PySimpleGUI as sg
 label = sg.Text("type in a to-do")
 input_box = sg.InputText(tooltip="enter todo")
 add_button=sg.Button("add")
-window = sg.Window("My to-Do App", layout=[[label], [input_box, add_button]])
+window = sg.Window("My to-Do App", layout=[label, input_box, add_button])
 window.read()
 window.close()
